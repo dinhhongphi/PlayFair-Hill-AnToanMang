d672f3679f1b766c0f87005c69d0607fbc078f78
dinh hong phi <dinhhongphi@hotmail.com>
2017-04-16 10:10:57 +0700

    create Coordinate class

---
+++ MaHoa/src/Coordinate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaHoa
+{
+    public class Coordinate
+    {
+        public int I { get; set; }
+        public int J { get; set; }
+    }
+}
