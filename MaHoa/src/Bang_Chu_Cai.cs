cf7c100c94ff22ad2de191d0907fb677389ecedc
phuc-kun <www.narutocntt@gmail.com>
2017-04-17 15:52:09 +0700

    Modifier  Bang_Chu_Cai

--- MaHoa/src/Bang_Chu_Cai.cs
+++ MaHoa/src/Bang_Chu_Cai.cs
@@ -6,35 +6,22 @@ using System.Windows.Forms;
 
 namespace MaHoa
 {
-    static class Program
+    public static class Bang_Chu_cai
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
-    static class Bang_Chu_cai
-    {
-        static int GetPosition(char Chu_cai)
+        public static int GetPosition(char Chu_cai)
         {
             return (int)(Chu_cai) - 64;
         }
-        static char GetCharacter(int x)
+        public static char GetCharacter(int x)
         {
             return (char)(x + 64);
         }
-        static char Seed(int currentPos, int offset)
+        public static char Seed(int currentPos, int offset)
         {
             int c = (currentPos + offset) % 26;
             return (char)(c + 64);
         }
-        static string Tolist()
+        public static string Tolist()
         {
             string str = null;
             for (int i = 0; i < 26; i++)
