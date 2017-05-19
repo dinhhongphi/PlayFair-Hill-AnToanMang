51c03bd705d76efff75eb9463a9c6b3e80f3f0bc
dinh hong phi <dinhhongphi.uit.workshop@gmail.com>
2017-05-19 11:11:54 +0700

    add test modifier

--- MaHoaTests/src/HillTest.cs
+++ MaHoaTests/src/HillTest.cs
@@ -10,27 +10,17 @@ namespace MaHoaTests.src
         [TestMethod]
         public void Encrypt_Test()
         {
-            Hill a = new Hill(3)
-            {
-                Matrix = new HillKey(3)
-                {
-                    matrix = new char[3, 3]
-                    {
-                        {'N','H','R' },
-                        {'Q','Y','F' },
-                        { 'A','J','D'}
-                    },
-                    matrix_int = new Matrix_Int(3)
-                    {
-                        matrix = new int[3, 3]
-                        {
-                            {13,7,17 },
-                            {16,24,5 },
-                            {0,9,3 }
-                        }
-                    }
-                }
-            };
+            Hill a = new Hill(3);
+
+            a.Matrix.SetValueChar(0, 0, 'N');
+            a.Matrix.SetValueChar(0, 1, 'H');
+            a.Matrix.SetValueChar(0, 2, 'R');
+            a.Matrix.SetValueChar(1, 0, 'Q');
+            a.Matrix.SetValueChar(1, 1, 'Y');
+            a.Matrix.SetValueChar(1, 2, 'F');
+            a.Matrix.SetValueChar(2, 0, 'A');
+            a.Matrix.SetValueChar(2, 1, 'J');
+            a.Matrix.SetValueChar(2, 2, 'D');
             a.SetData("THANH PHO");
             var result = a.Encrypt();
             Assert.AreEqual("KELFJEIRN", result);
@@ -39,18 +29,17 @@ namespace MaHoaTests.src
         [TestMethod()]
         public void Encrypt_N_Char_Test_3()
         {
-            Hill a = new Hill(3)
-            {
-                Matrix = new HillKey(3)
-                {
-                    matrix = new char[3, 3]
-                    {
-                        {'N','H','R' },
-                        {'Q','Y','F' },
-                        { 'A','J','D'}
-                    }
-                }
-            };
+            Hill a = new Hill(3);
+
+            a.Matrix.SetValueChar(0, 0, 'N');
+            a.Matrix.SetValueChar(0, 1, 'H');
+            a.Matrix.SetValueChar(0, 2, 'R');
+            a.Matrix.SetValueChar(1, 0, 'Q');
+            a.Matrix.SetValueChar(1, 1, 'Y');
+            a.Matrix.SetValueChar(1, 2, 'F');
+            a.Matrix.SetValueChar(2, 0, 'A');
+            a.Matrix.SetValueChar(2, 1, 'J');
+            a.Matrix.SetValueChar(2, 2, 'D');
             a.SetData("THA");
             var result = a.Encrypt_N_Char(new char[] { 'T', 'H', 'A' });
             CollectionAssert.AreEqual(new char[] { 'K', 'E', 'L' }, result);
@@ -59,17 +48,12 @@ namespace MaHoaTests.src
         [TestMethod()]
         public void Encrypt_N_Char_Test_2()
         {
-            Hill a = new Hill(2)
-            {
-                Matrix = new HillKey(2)
-                {
-                    matrix = new char[2, 2]
-                    {
-                        {'Z','F'},
-                        {'U', 'H'}
-                    }
-                }
-            };
+            Hill a = new Hill(2);
+
+            a.Matrix.SetValueChar(0, 0, 'Z');
+            a.Matrix.SetValueChar(0, 1, 'F');
+            a.Matrix.SetValueChar(1, 0, 'U');
+            a.Matrix.SetValueChar(1, 1, 'H');
             a.SetData("Th");
             var result = a.Encrypt_N_Char(new char[] { 'T', 'H' });
             CollectionAssert.AreEqual(new char[] { 'Q', 'N' }, result);
