0759ab237c48bd37904727cc95938fcbd2a642de
dinh hong phi <dinhhongphi.uit.workshop@gmail.com>
2017-05-21 23:31:23 +0700

    write function decrypt playfair

--- MaHoa/src/PlayFair.cs
+++ MaHoa/src/PlayFair.cs
@@ -52,13 +52,19 @@ namespace MaHoa
         /// </summary>
         /// <param name="dau"></param>
         /// <param name="cuoi"></param>
+        /// <param name="isEncrypt">Default this is method for encrypt</param>
         /// <returns></returns>
 #if TEST
-        public string HoanDoi(Coordinate dau, Coordinate cuoi)
+        public string HoanDoi(Coordinate dau, Coordinate cuoi, bool isEncrypt = true)
 #else
-        private string HoanDoi(Coordinate dau, Coordinate cuoi)
+        private string HoanDoi(Coordinate dau, Coordinate cuoi,bool isEncrypt = true)
 #endif
         {
+            int value = 1;
+            if (!isEncrypt)
+            {
+                value = -1;
+            }
             //check coordinate
             if (!(dau.I < matrix.N_matrix && dau.I >= 0
                 && dau.J < matrix.N_matrix && dau.J >= 0
@@ -68,13 +74,13 @@ namespace MaHoa
             var result = "";
             if (dau.I == cuoi.I) //same row
             {
-                result += matrix.Get(dau.I, (dau.J + 1) % matrix.N_matrix);
-                result += matrix.Get(cuoi.I, (cuoi.J + 1) % matrix.N_matrix);
+                result += matrix.Get(dau.I, ((dau.J + value)+ matrix.N_matrix) % matrix.N_matrix);
+                result += matrix.Get(cuoi.I, ((cuoi.J + value) + matrix.N_matrix) % matrix.N_matrix);
             }
             else if (dau.J == cuoi.J) //same column
             {
-                result += matrix.Get((dau.I + 1) % matrix.N_matrix, dau.J);
-                result += matrix.Get((cuoi.I + 1) % matrix.N_matrix, cuoi.J);
+                result += matrix.Get(((dau.I + value) + matrix.N_matrix) % matrix.N_matrix, dau.J);
+                result += matrix.Get(((cuoi.I + value) + matrix.N_matrix) % matrix.N_matrix, cuoi.J);
             }
             else
             {
@@ -110,10 +116,11 @@ namespace MaHoa
         /// <returns>Cyper text</returns>
         public string Encrypt(string plainText)
         {
-            if (plainText.Length == 0)
-                throw new ArgumentException("plaintext is empty");
             //remove space 
             plainText = RemoveSpaceInString(plainText);
+
+            if (plainText.Length == 0)
+                throw new ArgumentException("plaintext is empty");
 
             if(plainText.Length % 2 != 0)
             {
@@ -125,6 +132,48 @@ namespace MaHoa
             while(i < plainText.Length)
             {
                 var temp = EncryptTwoCharacter(plainText[i], plainText[i + 1]);
+                result += temp ?? throw new ArgumentException("plain text invalid");
+                i += 2;
+            }
+            return result;
+        }
+#if TEST
+        public string DecryptTwoCharacter(char one, char two)
+#else
+        private string DecryptTwoCharacter(char one, char two)
+#endif
+        {
+            if (one == ' ' || two == ' ')
+                throw new ArgumentException();
+            //get coordinate of one and two
+            Coordinate c_one, c_two;
+            c_one = matrix.GetCoordinate(one);
+            c_two = matrix.GetCoordinate(two);
+            if (c_one == null || c_two == null)
+                throw new ArgumentException();
+
+            //encrypt two character
+            string result = HoanDoi(c_one, c_two,false);
+            return result;
+        }
+
+        public string Decrypt(string plainText)
+        {
+            //remove space 
+            plainText = RemoveSpaceInString(plainText);
+            if (plainText.Length == 0)
+                throw new ArgumentException("plaintext is empty");
+
+            if (plainText.Length % 2 != 0)
+            {
+                plainText += "X";
+            }
+            //split pair two character to encrypt
+            int i = 0;
+            string result = "";
+            while (i < plainText.Length)
+            {
+                var temp = DecryptTwoCharacter(plainText[i], plainText[i + 1]);
                 result += temp ?? throw new ArgumentException("plain text invalid");
                 i += 2;
             }
